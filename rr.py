20e47afed709f5173f0a1e9739604f7cc6bc6f1a
alisahin07 <alisahin172@gmail.com>
2024-06-16 00:40:27 +0300

    first commit

--- rr.py
+++ rr.py
@@ -4,85 +4,82 @@ import plotly.express as px
 
 st.set_page_config(page_title="Sales Dashboard", page_icon=":bar_chart:")
 
-# Excel dosyasını yükleyin
-file_path = 'Kitap1.xlsx'
-df = pd.read_excel(file_path)
+# ---- FILE UPLOADER ----
+st.sidebar.header("Please upload your Excel file:")
+uploaded_file = st.sidebar.file_uploader("Upload Excel file", type=["xlsx"])
 
-# ---- SIDEBAR ----
-st.sidebar.header("Please Filter Here:")
-city = st.sidebar.multiselect(
-    "Select the BANKA:",
-    options=df["BANKA"].unique(),
-    default=df["BANKA"].unique()
-)
+if uploaded_file:
+    # Excel dosyasını yükleyin
+    df = pd.read_excel(uploaded_file)
 
-# Filtreleme işlemi
-df_selection = df[df["BANKA"].isin(city)]
+    # ---- SIDEBAR ----
+    st.sidebar.header("Please Filter Here:")
+    city = st.sidebar.multiselect(
+        "Select the BANKA:",
+        options=df["BANKA"].unique(),
+        default=df["BANKA"].unique()
+    )
 
+    # Filtreleme işlemi
+    df_selection = df[df["BANKA"].isin(city)]
 
-# ---- MAINPAGE ----
-st.title(":bar_chart: Sales Dashboard")
-st.markdown("##")
+    # ---- MAINPAGE ----
+    st.title(":bar_chart: Sales Dashboard")
+    st.markdown("##")
 
-# TOP KPI's
-total_sales = int(df_selection["TUTAR"].sum())
-average_sale_by_transaction = round(df_selection["TUTAR"].mean(), 2)
-left_column, right_column = st.columns(2)
-with left_column:
-    st.subheader("Toplam Tutar:")
-    st.subheader(f"TL ₺ {total_sales:,}")
+    # TOP KPI's
+    total_sales = int(df_selection["TUTAR"].sum())
+    average_sale_by_transaction = round(df_selection["TUTAR"].mean(), 2)
+    left_column, right_column = st.columns(2)
+    with left_column:
+        st.subheader("Toplam Tutar:")
+        st.subheader(f"TL ₺ {total_sales:,}")
 
-with right_column:
-    st.subheader("Bankaların Ortalama TuTarı")
-    st.subheader(f"₺ {average_sale_by_transaction:,.2f}")
+    with right_column:
+        st.subheader("Bankaların Ortalama TuTarı")
+        st.subheader(f"₺ {average_sale_by_transaction:,.2f}")
 
-st.markdown("---")
+    st.markdown("---")
 
-# Filtrelenmiş DataFrame'i göster
+    # Bankalara göre tutar
+    bankalara_gore_tutar = (
+        df_selection.groupby(by=["BANKA"])[["TUTAR"]].sum().sort_values(by="TUTAR")
+    )
 
+    fig_product_sales = px.bar(
+        bankalara_gore_tutar,
+        x="TUTAR",
+        y=bankalara_gore_tutar.index,
+        orientation="h",
+        title="<b>Sales by Bank</b>",
+        color_discrete_sequence=["#0083B8"] * len(bankalara_gore_tutar),
+        template="plotly_white",
+    )
 
-# Bankalara göre tutar
-bankalara_gore_tutar = (
-    df_selection.groupby(by=["BANKA"])[["TUTAR"]].sum().sort_values(by="TUTAR")
-)
+    st.markdown("##")
 
-fig_product_sales = px.bar(
-    bankalara_gore_tutar,
-    x="TUTAR",
-    y=bankalara_gore_tutar.index,
-    orientation="h",
-    title="<b>Sales by Bank</b>",
-    color_discrete_sequence=["#0083B8"] * len(bankalara_gore_tutar),
-    template="plotly_white",
-)
+    # TOP KPI's
 
+    left_column, right_column = st.columns(2)
+    with left_column:
+        st.subheader("Banka Grafikler:")
+        st.plotly_chart(fig_product_sales, use_container_width=True)
 
+    with right_column:
+        st.subheader("Excell Verisi")
+        st.dataframe(df_selection, use_container_width=True)
 
+    st.markdown("---")
 
+    # ---- HIDE STREAMLIT STYLE ----
+    hide_st_style = """
+                <style>
+                #MainMenu {visibility: hidden;}
+                footer {visibility: hidden;}
+                header {visibility: hidden;}
+                </style>
+                """
 
-
-st.markdown("##")
-
-# TOP KPI's
-
-left_column, right_column = st.columns(2)
-with left_column:
-    st.subheader("Banka Grafikler:")
-    st.plotly_chart(fig_product_sales)
-
-with right_column:
-    st.subheader("Excell Verisi")
-    st.dataframe(df_selection)
-
-st.markdown("---")
-
-# ---- HIDE STREAMLIT STYLE ----
-hide_st_style = """
-            <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            header {visibility: hidden;}
-            </style>
-            """
-
-st.markdown(hide_st_style, unsafe_allow_html=True)
+    st.markdown(hide_st_style, unsafe_allow_html=True)
+else:
+    st.warning("Please upload an Excel file.")
